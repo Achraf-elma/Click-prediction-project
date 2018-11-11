4fa25b24c8012a98db6553a0494540371262125f
achraf-elma <achrafelmahmoudi@gmail.com>
2018-11-11 16:41:01 +0100

    clean network

--- src/main/scala/Cleaner.scala
+++ src/main/scala/Cleaner.scala
@@ -32,5 +32,30 @@ object Cleaner{
     }}
   }
 
+def udf_clean_network = {
+    udf {(s: String) =>
+      /*if(list.contains("["+s+"]")){
+            defaultValue
+          }
+          else{
+            s
+          }*/
+      val ethernet = new Regex("1(.*)")
+      val wifi = new Regex("2(.*)")
+      val cell1 = new Regex("3(.*)")
+      val cell2 = new Regex("4(.*)")
+      val cell3 = new Regex("5(.*)")
+      val cell4 = new Regex("6(.*)")
 
+      s match {
+        case ethernet(x) => "Ethernet"
+        case wifi(x) => "Wifi"
+        case cell1(x) => "Cellular Network Unknown"
+        case cell2(x) => "2G"
+        case cell3(x) => "3G"
+        case cell4(x) => "4G"
+        case _ => "Unknown"
+      }
+    }
+  }
 }
