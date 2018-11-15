f95a493231ce99fee39f060f16554726fab1b970
Hamelina <ehamelojulia@gmail.com>
2018-11-15 22:13:43 +0100

    change source

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -27,7 +27,7 @@ object Main extends App{
   import org.apache.spark.sql.functions._
 
   //select your variable to add and change inside the variable columnVectorialized and dataModel at the end of the code
-  val untreatedData = context.read.json("./src/main/scala/data-students.json").select("appOrSite", "network", "type", "publisher","size", "label", "interests", "user")
+  val untreatedData = context.read.json("./src/resources/data-students.json").select("appOrSite", "network", "type", "publisher","size", "label", "interests", "user")
 
   val df = untreatedData.withColumn("label", when(col("label") === true, 1).otherwise(0))
     .withColumn("network", Cleaner.udf_clean_network(untreatedData("network")))
