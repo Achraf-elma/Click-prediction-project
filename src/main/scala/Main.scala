737cd1c1499f97425a02aa3eae65f5e0e17e0022
Hamelina <ehamelojulia@gmail.com>
2018-11-22 08:17:49 +0100

    add metrics

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,10 @@
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{OneHotEncoderEstimator, StringIndexer, VectorAssembler}
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
-import org.apache.spark.ml.feature.{StringIndexer, OneHotEncoderEstimator, VectorAssembler}
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.{DataFrame}
-import org.apache.spark.sql.types.{LongType, StructField, StructType}
 
 /**
   * The programm that predict if a user clicks on an or not
@@ -27,7 +24,7 @@ object Main extends App{
   import org.apache.spark.sql.functions._
 
   //select your variable to add and change inside the variable columnVectorialized and dataModel at the end of the code
-  val untreatedData = context.read.json(args(0)).select("appOrSite", "network", "type", "publisher","size", "label", "interests", "user")
+  val untreatedData = context.read.json("./src/main/scala/data-students.json").select("appOrSite", "network", "type", "publisher","size", "label", "interests", "user")
 
   val df = untreatedData.withColumn("label", when(col("label") === true, 1).otherwise(0))
     .withColumn("network", Cleaner.udf_clean_network(untreatedData("network")))
@@ -105,7 +102,7 @@ object Main extends App{
   cvModel.write.overwrite().save("model")
   println("Model created & saved")
 
-  val testData = dataModel.limit(100)
+  val testData = dataModel.limit(1000)
 
   // Prediction
   println("Predection with first 100 rows :")
@@ -124,6 +121,32 @@ object Main extends App{
     .foreach { case Row(features: Vector, prob: Vector, prediction: Double) =>
       println(s"($features) --> prob = $prob, prediction = $prediction")
     }
+
+  val lp = predictions.select( "label", "prediction")
+  val counttotal = predictions.count()
+  val correct = lp.filter(col("label") === col("prediction")).count()
+  val wrong = lp.filter(not(col("label") === col("prediction"))).count()
+  val truep = lp.filter(col("prediction") === 0.0).filter(col("label") === col("prediction")).count()
+  val falseN = lp.filter(col("prediction") === 0.0).filter(not(col("label") === col("prediction"))).count()
+  val falseP = lp.filter(col("prediction") === 1.0).filter(not(col("label") === col("prediction"))).count()
+  val ratioWrong=wrong.toDouble/counttotal.toDouble
+  val ratioCorrect=correct.toDouble/counttotal.toDouble
+  //precision: vraiPositif/(vrai positif+faux positif)
+  //rappel: vrai positif/ (vrai positif+faux négatifs)
+  val accuracy = truep.toDouble/(truep.toDouble + falseN.toDouble)
+  val recall = truep.toDouble / (truep.toDouble + falseP.toDouble)
+
+  println("Total line: " + counttotal)
+  println("Total of correct answer" + correct)
+  println("Total of wrong: " + wrong)
+  println("True positif: " + truep)
+  println("False negatif: " + falseN)
+  println("False positif: " + falseP)
+  println("accuracy: " + ratioWrong)
+  println("recall: " + ratioCorrect)
+  println("accuracy: " + accuracy)
+  println("recall: " + recall)
+
 
   val evaluator = cv.getEvaluator
 
